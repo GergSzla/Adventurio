21dcbb5e14ca10ecd83d55621d5916dadeab58df
gergo <gergo.szla@gmail.com>
2020-01-29 15:47:26 +0000

    Files Added

---
+++ app/src/main/java/ie/wit/adventurio/activities/RegisterActivity.kt
@@ -0,0 +1,13 @@
+package ie.wit.adventurio.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ie.wit.adventurio.R
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
